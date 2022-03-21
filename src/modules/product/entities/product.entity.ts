e6b7d4e215ff6644caf22d1497956774e8df1577
Egor <48442694+ShonS1v4@users.noreply.github.com>
2022-03-21 12:34:06 +0300

    release_1.2.1
    FIX:
    Swagger works again 2

--- src/modules/product/entities/product.entity.ts
+++ src/modules/product/entities/product.entity.ts
@@ -1,18 +1,14 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { Column, Model, Table } from 'sequelize-typescript';
 import { DataType } from 'sequelize-typescript';
 
 @Table
 export class Product extends Model {
   @Column({ type: DataType.INTEGER, autoIncrement: true, primaryKey: true })
-  @ApiProperty({ example: 1 })
   id: number;
 
   @Column
-  @ApiProperty({ example: 'Product name' })
   name: string;
 
   @Column
-  @ApiProperty({ example: 100 })
   stock: number;
 }
